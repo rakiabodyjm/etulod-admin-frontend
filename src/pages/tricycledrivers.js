6248d8298a307938fc59d6b9248bc981e6983006
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-09-08 11:43:45 +0800

    Initialize Admin Panel

---
+++ src/pages/tricycledrivers.js
@@ -0,0 +1,9 @@
+import { Typography } from "@material-ui/core";
+
+export default function TDManagementPage() {
+  return (
+    <div>
+      <Typography>This is TD Management Page</Typography>
+    </div>
+  );
+}
---
+++ src/pages/users.js
@@ -0,0 +1,9 @@
+import { Typography } from "@material-ui/core";
+
+export default function UserManagementPage() {
+  return (
+    <div>
+      <Typography>User Management Page</Typography>
+    </div>
+  );
+}
